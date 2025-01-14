1983b23502c51624e608342d80026cb03a0d6410
Felix-73 <felix.gueydan@gmail.com>
2025-01-14 13:13:23 +0000

    New Sample-Web

--- Flask/app.py
+++ Flask/app.py
@@ -5,26 +5,45 @@ from werkzeug.serving import run_simple
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 
-##settings 
+# Déterminer les chemins en fonction de l'environnement
+if "SNAP" in os.environ:
+    # Dans le snap
+    STATIC_FOLDER = os.path.join(os.environ['SNAP'], 'bin', 'Flask', 'static')
+    TEMPLATE_FOLDER = os.path.join(os.environ['SNAP'], 'bin', 'Flask', 'templates')
+else:
+    # En développement local
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    STATIC_FOLDER = os.path.join(dir_path, 'static')
+    TEMPLATE_FOLDER = os.path.join(dir_path, 'templates')
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-print(dir_path)
-app = Flask(__name__, static_url_path='')
+# Création de l'application Flask avec les bons chemins
+app = Flask(__name__,
+           static_folder=STATIC_FOLDER,
+           template_folder=TEMPLATE_FOLDER,
+           static_url_path='/sample-web/static')
+
+# Debug des chemins
+print("Static folder:", app.static_folder)
+print("Template folder:", app.template_folder)
 
 app.wsgi_app = ProxyFix(
     app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
 )
 
-os.chdir(dir_path)
-
-
 
 ########## serving functions
 
-@app.route('/sample-flask/')
+@app.route('/sample-web')
 def index():
-    
-    return app.send_static_file('sample-flask/index.html')
+    return render_template('index.html')
+
+@app.route('/sample-web/page1')
+def page1():
+    return render_template('page1.html')
+
+@app.route('/sample-web/graphique')
+def page2():
+    return render_template('graphique.html')
 
 ##server start
 
@@ -32,9 +51,8 @@ if __name__ == '__main__':
   
 
     if "SNAP_DATA" in os.environ:
-        run_simple('unix://'+os.environ['SNAP_DATA']+'/package-run/sample-flask-webapp/example.sock', 0, app)
+        run_simple('unix://'+os.environ['SNAP_DATA']+'/package-run/sample-web/example.sock', 0, app)
         #app.run(host='0.0.0.0',debug = False, port=3125)
     else:
-
         app.run(host='0.0.0.0',debug = False, port=12121)
 
