654ef6134463b043cbbb0731340f3f9df49f59a7
Felix-73 <felix.gueydan@gmail.com>
2025-07-16 09:04:50 +0000

    minor change

--- Flask/static/js/lineChart.js
+++ Flask/static/js/lineChart.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const ctx = document.getElementById('lineChart').getContext('2d');
     new Chart(ctx, {
         type: 'line',
         data: {
-            labels: ['Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam', 'Dim'],
+            labels: ['10', '20', '30', '40', '50', '60', '70'],
             datasets: [{
-                label: 'Activité',
+                label: 'Data',
                 data: [65, 59, 80, 81, 56, 55, 40],
                 borderColor: 'rgba(153, 102, 255, 1)',
                 tension: 0.1
